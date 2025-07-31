4ec8240c668737a37324ded2cfe1b7aa1d74e10e
earth nut dev <earthnut.dev@outlook.com>
2025-07-31 23:00:21 +0800

    version : v0.1.1 2025/7/31 23:00:21

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -5,7 +5,7 @@ CHECK_VERSION="@qqi/check-version"
 install_check_version() {
     if ! npm  list -g --depth=0 | grep -q "${CHECK_VERSION}"; then 
         echo "当前未全局安装 '${CHECK_VERSION}'，即将进行安装"
-        pnpm add ${CHECK_VERSION} --global
+        npm install ${CHECK_VERSION} --global
     else 
          echo "包 ${CHECK_VERSION} 已全局安装"
     fi
@@ -14,7 +14,7 @@ install_check_version() {
 tag=""
 # npx 在某种程度上还是要比 pnpm 更靠谱呀
 install_check_version
-if ! tag=$(npx  "${CHECK_VERSION}" c=. 2>&1); then
+if ! tag=$(npx "${CHECK_VERSION}" c=. 2>&1); then
     echo "未通过版本校验：$tag"
     exit 0
 fi
