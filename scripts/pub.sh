b55137685d3371f8e21bee94d7ba2e337ef80b6c
earth nut dev <earthnut.dev@outlook.com>
2025-07-31 22:29:22 +0800

    submit :  2025/7/31 22:29:22

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 CHECK_VERSION="@qqi/check-version"
 
-printf $(pnpm dlx "${CHECK_VERSION}" -v)  # 更改全局安装的测试方法
+printf $(pnpm add  "${CHECK_VERSION}" -g)  # 更改全局安装的测试方法
 tag=""
-if ! tag=$(pnpm dlx "${CHECK_VERSION}" c=. 2>&1); then
+if ! tag=$("${CHECK_VERSION}" c=. 2>&1); then
     echo "未通过版本校验：$tag"
     exit 0
 fi
